479d6e603d7170ef34f3df95625877398c94d5ad
lcs <lcs@lcs.com>
2014-04-14 18:03:52 +0800

    haha

--- wp-content/themes/oplan/header.php
+++ wp-content/themes/oplan/header.php
@@ -1,5 +1,5 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes() ?>>
+<!DOCTYPE html>
+<html <?php language_attributes() ?>>
 <head profile="http://gmpg.org/xfn/11">
 <title><?php bloginfo('name'); if ( is_404() ) : _e(' &raquo; ', 'sandbox'); _e('Not Found', 'sandbox'); elseif ( is_home() ) : _e(' &raquo; ', 'sandbox'); bloginfo('description'); else : wp_title(); endif; ?></title>
 <meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
@@ -22,11 +22,14 @@
 <div id="wrapper" class="hfeed">
 
 	<div id="header">
-		<h1 id="blog-title"><a href="<?php echo get_option('home') ?>/" title="<?php bloginfo('name') ?>" rel="home"><?php bloginfo('name'); ?></a></h1>
+		<h1 id="blog-title">
+			<a href="<?php echo get_option('home') ?>/" title="<?php bloginfo('name') ?>" rel="home">
+				<?php bloginfo('name'); ?>
+			</a>
+		</h1>
 		<div id="blog-description"><?php bloginfo('description') ?></div>
 	</div><!--  #header -->
 
-	<div id="access">
-		<div class="skip-link"><a href="#content" title="<?php _e('Skip navigation to the content', 'sandbox'); ?>"><?php _e('Skip to content', 'sandbox'); ?></a></div>
-		<?php sandbox_globalnav() ?>
-	</div><!-- #access -->
+	<div id="nav">
+		<?php wp_list_pages();?>
+	</div><!-- #nav -->
