44debce71c290f1004a8f2f197a783ee63296c42
lcs <lcs@lcs.com>
2014-04-15 18:03:29 +0800

    shift

--- wp-content/themes/aoplan/footer.php
+++ wp-content/themes/aoplan/footer.php
@@ -12,5 +12,10 @@
 
 <?php wp_footer(); ?>
 
+<!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
 </body>
 </html>
--- wp-content/themes/aoplan/header.php
+++ wp-content/themes/aoplan/header.php
@@ -5,15 +5,19 @@
 <meta http-equiv="content-type" content="<?php bloginfo('html_type') ?>; charset=<?php bloginfo('charset') ?>" />
 <meta name="description" content="<?php bloginfo('description') ?>" />
 <meta name="generator" content="WordPress <?php bloginfo('version') ?>" /><!-- Please leave for stats -->
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-<style type="text/css" media="all"> @import url(<?php bloginfo('template_url'); ?>/css/reset.css); @import url(<?php bloginfo('template_url'); ?>/css/generic.css); </style>
-<style type="text/css" media="print"> @import url(<?php bloginfo('template_url'); ?>/css/print.css); </style>
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
 <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('rss2_url') ?>" title="<?php echo wp_specialchars(get_bloginfo('name'), 1) ?> <?php _e('Posts RSS feed', 'sandbox'); ?>" />
 <link rel="alternate" type="application/rss+xml" href="<?php bloginfo('comments_rss2_url') ?>" title="<?php echo wp_specialchars(get_bloginfo('name'), 1) ?> <?php _e('Comments RSS feed', 'sandbox'); ?>" />
 <link rel="pingback" href="<?php bloginfo('pingback_url') ?>" />
 
-<?php wp_head() ?>
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="http://1.gtbcode.sinaapp.com/css/style.css">
+
+<?php wp_enqueue_script( array('jquery'));wp_head() ?>
 
 </head>
 
@@ -30,6 +34,10 @@
 		<div id="blog-description"><?php bloginfo('description') ?></div>
 	</div><!--  #header -->
 
-	<div id="nav">
-		<?php wp_list_pages();?>
-	</div><!-- #nav -->
+	<header class="navbar navbar-static-top bs-docs-nav" >
+
+		<?php
+			$page = wp_nav_menu( array("echo"=>false,"depth"=>2 , "menu_class"=>"nav","container_class"=>"abs"));
+			echo "$page";
+		?>
+	</header><!-- #nav -->
