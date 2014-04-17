dbea5ceb179d6d6c2681a6071025c9b0824af6cc
LiJohnson <a598420668@gmail.com>
2014-04-17 23:55:37 +0800

    添加onetone主题

--- wp-content/themes/aoplan/page.php
+++ wp-content/themes/aoplan/page.php
@@ -1,16 +1,15 @@
 <?php get_header() ?>
-<?php the_title(); ?>
 <?php the_post(); ?>
-
 	<div id="container">
 		<div id="content">
 			<div id="post-<?php the_ID(); ?>" >
+				<div class="entry-meta" style="float:right;">
+					<?php edit_post_link(__('Edit', 'sandbox'),'<span class="edit-link">','</span>') ?>
+				</div>
 				<h2 class="entry-title"><?php the_title(); ?></h2>
+				
 				<div class="entry-content">
 					<?php the_content() ?>
-				</div>
-				<div class="entry-meta">
-					<?php edit_post_link(__('Edit', 'sandbox'),'<span class="edit-link">','</span>') ?>
 				</div>
 			</div><!-- .post -->
 		</div><!-- #content -->
