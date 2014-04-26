87b158a228c39b257d692974fd20c112d0d5c521
LiJohnson <598420668@163.com>
2014-04-26 15:39:18 +0800

    修改滚动的偏移

--- wp-content/themes/aoplan/js/aoplan.js
+++ wp-content/themes/aoplan/js/aoplan.js
@@ -1,8 +1,9 @@
 jQuery(function($){
-	$("body").scrollspy({ target: '.home-header' });
+	var offset =  80 + ($("#wpadminbar").height()||0);
+	$("body").scrollspy({ target: '.home-header' , offset : offset });
 	$(".home-header .nav li").click(function(){
 		var $content = $($(this).find("a").attr("href"));
-		$("body").animate({scrollTop:$content.offset().top - 80 - ($("#wpadminbar").height()||0)});
+		$("body").animate({scrollTop:$content.offset().top - offset });
 		return false;
 	});
 });
