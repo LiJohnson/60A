6a8e489bf249df60df578805242bc37d3c7356a8
NoteBook <598420668@163.com>
2014-04-30 00:21:56 +0800

    shitter

--- wp-content/themes/aoplan/home.php
+++ wp-content/themes/aoplan/home.php
@@ -8,8 +8,11 @@
 				foreach (wp_get_nav_menu_items("aoplan",array()) as $key => $page) {
 					echo "<div id='aop-menu-item-$page->ID' class='aoplan-page'>";
 					echo "<h2>$page->post_content </h2><div class='page-content'>$page->post_content </div>";
-					$page = get_page($page->ID);
+					var_dump($page);
+					$page = get_post($page->object_id);
 					var_dump($page->post_content);
+					var_dump(apply_filters('the_content', $page->post_content));
+					the_post();
 					echo "</div>";
 				}
 			?>
